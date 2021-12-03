b4e35b8f5346c04d2b7c43b2c89321deb6c2d11e
Your Name <you@example.com>
2021-12-03 10:20:36 +0700

    update

--- src/login.js
+++ src/login.js
@@ -1,119 +1,153 @@
 import React from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    TextInput,
-    ImageBackground
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Alert,
+  ToastAndroid,
+  Modal,
+  Image,
+  ImageBackground,
 } from 'react-native';
-
+import MashButton from './CustomButton';
 
 
 const LoginScreen = ({navigation}) =>{
+  const [name, SetName] = useState('');
+  const [submitted, SetSubmitted] = useState(false);
+  const [showWarning, SetshowWarning] = useState(false);
+  const onPressHandler = () => {
+    if (name.length > 3) {
+      SetSubmitted(!submitted);
+    } else {
+      SetshowWarning(true);
+    }
+  }
+
+
     return (
-        <View style={styles.container}>
-          <ImageBackground  style={styles.imgbg}
-          source = {require('../assets/background.png')}        
-          >              
-            <Text style={styles.text}>ĐĂNG NHẬP</Text>
-            <View style={styles.inputContainer}>
-              <TextInput
-                placeholder ="Tên đăng nhập "
-                style ={styles.input}
-                >
-              </TextInput>
-
-              <TextInput
-                placeholder ="Mật khẩu"
-                style ={styles.input}
-                secureTextEntry
-              >
-              </TextInput>
+      <ImageBackground
+      style={styles.body}
+      source = {require('../assets/background.png')}
+    >
+      <Header />
+      <Modal
+        visible={showWarning}
+        transparent
+        onRequestClose={() =>
+          SetshowWarning(false)
+        }
+        animationType='slide'
+        hardwareAccelerated
+      >
+        <View style={styles.centered_view}>
+          <View style={styles.warning_modal}>
+            <View style={styles.warning_title}>
+              <Text style={styles.text}>WARNING!</Text>
             </View>
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.4}
-                onPress={() => navigation.navigate('HomeScreen')}
-              >
-                <Text style={styles.buttonText} >Sign in</Text>
-              </TouchableOpacity>
-
-
-              <TouchableOpacity
-                activeOpacity={0.1}
-                onPress={() => navigation.navigate('HomeScreen')}
-              >
-                <Text style={styles.linktext1}   >Don't have account ?   Sign up </Text> 
-
-              </TouchableOpacity>
+            <View style={styles.warning_body}>
+              <Text style={styles.text}>The name must be longer than 3 charachters</Text>
             </View>
-
-          </ImageBackground>
-          
+            <Pressable
+              onPress={() => SetshowWarning(false)}
+              style={styles.warning_button}
+              android_ripple={{ color: '#fff' }}
+            >
+              <Text style={styles.text}>OK</Text>
+            </Pressable>
+          </View>
         </View>
+      </Modal>
+      <Text style={styles.text}>
+        Email
+      </Text>
+      
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. John'
+        onChangeText={(value) => SetName(value)}
+      />
+      <Text style={styles.text}>
+        Password
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder='********'
+        onChangeText={(value) => SetName(value)}
+      />
+
+      <MashButton
+        onPressFunction={onPressHandler}
+        title={'Sign-in'}
+        color={'#00ff00'}
+      />
+    </ImageBackground >
     );
 };
 const styles = StyleSheet.create({
-  container: {
+   body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-  imgbg :{
-    height : '100%',
-    width :'100%' , 
-    alignItems :'center',
-  },
-  text :{
-    fontWeight : 'bold',
-    color : '#fff',
-    fontFamily :'Open Sans',
-    fontSize : 40,
-    marginTop : 100,
-  },
-  inputContainer :{
-    marginTop: 10,
-    justifyContent: 'center',
     alignItems: 'center',
   },
-  input :{
-    backgroundColor :'#ffffff10',
-    paddingHorizontal :10 ,
-    paddingVertical :10 ,
-    borderRadius :10,
-    marginTop : 15,
-    borderWidth :2 ,
+  text: {
+    color: '#000000',
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
   },
-  button :{
-    backgroundColor :'#999966',
-    alignItems :'center',
-    borderRadius :10,
-    minHeight: 60,
-    height: 60,
-    minWidth: 100,
+  input: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#555',
+    borderRadius: 5,
+    textAlign: 'center',
+    fontSize: 20,
+    marginBottom: 10,
   },
-  buttonContainer :{
-    justifyContent : 'center',
-    alignItems :'center',
-    marginTop :30,
+  button: {
+    width: 150,
+    height: 50,
+    alignItems: 'center',
   },
-  buttonText :{
-    color :'blue',
-    fontWeight : '400',
-    fontSize :16,
-    marginTop: 20 ,
+  centered_view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00000099'
   },
-  linktext1 :{
-    textDecorationLine: 'underline',
-    color :'red',
-    fontSize : 13,
+  warning_modal: {
+    width: 300,
+    height: 300,
+    backgroundColor: '#ffffff',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 20,
   },
-
-
+  warning_title: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ff0',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  warning_body: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  warning_button: {
+    backgroundColor: '#00ffff',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 10,
+  }
 });
   
 export default LoginScreen;
